4606e62d97a944375f2828fd7ef5f59799ce875e
hernani <hernani.luiz@gmail.com>
2018-06-01 15:45:58 -0300

    Ident print boleto

--- src/components/steps/printBoleto.vue
+++ src/components/steps/printBoleto.vue
@@ -1,7 +1,7 @@
 <template>
-		<section v-if="messages" class="certiface-verify">
-			<div class="input-wrapper" v-html="messages[0].text"></div>
-			<div class="input-wrapper" v-if="messages[1]">
+		<section v-if="messages" class="certiface-verify" v-cloak>
+			<div class="input-wrapper" v-if="messages[0]"  v-html="messages[0].text" v-cloak></div>
+			<div class="input-wrapper" v-if="messages[1]" >
 				<a target="_blank" class="donation-nav donation-nav--forward" @click.prevent="redirectUser(messages[1])" >{{messages[1].text}} </a>
 			</div>
 		</section>
@@ -9,45 +9,44 @@
 
 <script>
 
-	export default {
-		name: 'printBoleto',
-		data() {
-			return {
-				loading: false,
-				errorMessage: '',
+export default {
+  name: 'printBoleto',
+  data() {
+    return {
+      loading: false,
+      errorMessage: '',
 
-				validation: {
-					errors: {},
-				},
-			};
-		},
-		computed: {
-			username() {
-				return this.$store.state.username;
-			},
-			messages() {
-				return this.$store.state.messages.messages;
-			},
-		},
-		methods: {
-			toggleLoading() {
-				this.loading = !this.loading;
-			},
-			handleErrorMessage(err) {
-				this.errorMessage = err.data[0].message;
-			},
-			redirectUser(ui) {
-				if( ui.value === "pay_with_cc" ){
-					this.$router.replace(this.$route.path);
-						this.$store.dispatch("CHANGE_PAYMENT_STEP", {
-							step: "userData"
-					});
-				}else{
-					sessionStorage.clear();
-					window.open(ui.href)
-				}
-			}
-		},
-
-	};
+      validation: {
+        errors: {},
+      },
+    };
+  },
+  computed: {
+    username() {
+      return this.$store.state.username;
+    },
+    messages() {
+      return this.$store.state.messages;
+    },
+  },
+  methods: {
+    toggleLoading() {
+      this.loading = !this.loading;
+    },
+    handleErrorMessage(err) {
+      this.errorMessage = err.data[0].message;
+    },
+    redirectUser(ui) {
+      if (ui.value === 'pay_with_cc') {
+        this.$router.replace(this.$route.path);
+        this.$store.dispatch('CHANGE_PAYMENT_STEP', {
+          step: 'userData',
+        });
+      } else {
+        sessionStorage.clear();
+        window.open(ui.href);
+      }
+    },
+  },
+};
 </script>
