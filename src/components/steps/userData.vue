f9d2b93fdd3739d9fb9a6236f960d0fee649d825
hernani <hernani.luiz@gmail.com>
2018-06-08 14:28:14 -0300

    Add alert in change step user data

--- src/components/steps/userData.vue
+++ src/components/steps/userData.vue
@@ -170,7 +170,9 @@ export default {
             amount: this.amount,
           }),
         );
+        alert('antes');
 		 this.$store.dispatch('CHANGE_PAYMENT_STEP', { step: 'address' });
+		 alert('depois');
 		  const payload = {
 		  nameJoin,
           cpf,
