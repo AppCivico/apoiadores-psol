18eb1de38dace5d987f6327649e93ed49d78382b
Evelyn Perez <evelyn@eokoe.com>
2018-09-19 16:29:12 -0300

    Indentations to spaces on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,22 @@
 <template>
   <main id="conteudo">
-		<article class="home__intro">
-			<div class="container">
+    <article class="home__intro">
+      <div class="container">
         <h2>
           Ajude a mudar o brasil!
         </h2>
-
         <p>
-			<span class="bigger-text">Por que financiar o PSOL e sua aliança com movimentos sociais?</span> Em tempos de tantos escândalos, somos um dos poucos partidos sem envolvimento em nenhuma denúncia de corrupção. Nossa bancada no Congresso é reconhecidamente a melhor do país, sempre atuando com independência, ética e coerência. Nunca recebemos dinheiro das grandes empresas e lutamos pelo fim do financiamento privado de campanhas.
-		</p>
+          <span class="bigger-text">Por que financiar o PSOL e sua aliança com movimentos sociais?</span> Em tempos de tantos escândalos, somos um dos poucos partidos sem envolvimento em nenhuma denúncia de corrupção. Nossa bancada no Congresso é reconhecidamente a melhor do país, sempre atuando com independência, ética e coerência. Nunca recebemos dinheiro das grandes empresas e lutamos pelo fim do financiamento privado de campanhas.
+        </p>
         <p>
-			<span class="bigger-text">O que queremos?</span> Consolidar, em cada canto do país, uma forte aliança contra as desigualdades e pela democracia. Boulos e Sônia são porta vozes da mudança que o Brasil precisa.
-		</p>
+          <span class="bigger-text">O que queremos?</span> Consolidar, em cada canto do país, uma forte aliança contra as desigualdades e pela democracia. Boulos e Sônia são porta vozes da mudança que o Brasil precisa.
+        </p>
         <p>
-			<span class="bigger-text">O dinheiro vai ser usado para quê?</span> Para a construção aberta das propostas que vamos apresentar nessas eleições, com Guilherme Boulos e Sonia Guajajara. Serão realizados debates abertos em todo o país, com participação de especialistas - e tudo consolidado em uma plataforma na internet. E não se constrói um programa que proponha mudanças profundas sem viajar para todos os cantos do país, conversando com trabalhadores urbanos e rurais, desempregados, jovens da periferia e das universidades, mulheres, negros, lgbts, ou seja, ouvindo e discutindo a mudança olho no olho.
-
-		</p>
+          <span class="bigger-text">O dinheiro vai ser usado para quê?</span> Para a construção aberta das propostas que vamos apresentar nessas eleições, com Guilherme Boulos e Sonia Guajajara. Serão realizados debates abertos em todo o país, com participação de especialistas - e tudo consolidado em uma plataforma na internet. E não se constrói um programa que proponha mudanças profundas sem viajar para todos os cantos do país, conversando com trabalhadores urbanos e rurais, desempregados, jovens da periferia e das universidades, mulheres, negros, lgbts, ou seja, ouvindo e discutindo a mudança olho no olho.
+        </p>
         <p>
-			<span class="bigger-text">Eu posso participar?</span> A contribuição financeira é apenas uma das formas de participar. Nossa construção é coletiva, de baixo para cima. Você pode participar dos eventos de construção do programa, constituir um grupo de ação, divulgar nossos materiais, sugerir ações. Queremos que sejas protagonista da mudança. <span class="bigger-text">Vamos?</span>
-		</p>
+          <span class="bigger-text">Eu posso participar?</span> A contribuição financeira é apenas uma das formas de participar. Nossa construção é coletiva, de baixo para cima. Você pode participar dos eventos de construção do programa, constituir um grupo de ação, divulgar nossos materiais, sugerir ações. Queremos que sejas protagonista da mudança. <span class="bigger-text">Vamos?</span>
+        </p>
         <section id="campaign-progress" class="campaign-progress">
           <p>
             <span class="currency">R$</span>
@@ -27,8 +25,7 @@
               <template v-else>0</template>
             </strong>
           </p>
-		  <p class="campaign-progress-amount">{{ candidate.people_donated }} doações realizadas</p>
-
+          <p class="campaign-progress-amount">{{ candidate.people_donated }} doações realizadas</p>
           <progress :value="candidate.total_donated" :max="expected">
             <div class="progress-bar">
               <span :style="{width: `${porcentage}%`}">
@@ -36,12 +33,9 @@
               </span>
             </div>
           </progress>
-
           <p class="campaign-progress-porcentage">
             {{ porcentage }}% da meta de R$ {{ expected | formatBRL }}
           </p>
-
-
         </section>
       </div>
     </article>
@@ -57,11 +51,12 @@
         <h2>
           Em que o dinheiro arrecadado vai ser usado?
         </h2>
-
-        <p><strong>Viabilizar a realização de eventos nas 5 regiões do país, em 17 cidades, para a construção do programa.</strong>
+        <p>
+          <strong>Viabilizar a realização de eventos nas 5 regiões do país, em 17 cidades, para a construção do programa.</strong>
           Vamos construir junto aos movimentos sociais eventos abertos para que todas e todos possam debater e propor suas ideias para o Brasil, nas mais diversas temáticas. Para isso, é necessário viabilizar a estrutura necessária para os debates, bem como o deslocamento de debatedores e a cobertura de comunicação. É o maior custo dessa proposta.
         </p>
-        <p><strong>Manutenção de plataforma online para participação e mobilização, especialmente os grupos de ação.</strong>
+        <p>
+          <strong>Manutenção de plataforma online para participação e mobilização, especialmente os grupos de ação.</strong>
           Nossa plataforma online será totalmente focada na participação: será possível cadastrar um grupo pequeno, em sua cidade ou bairro, para debater e construir nossa pré-campanha, realizar eventos e se articular com outros grupos e pessoas. Além disso, você poderá fazer propostas e debater as que outras pessoas estarão propondo.
         </p>
         <picture>
@@ -71,15 +66,16 @@
           <source media="(max-width: 800px)" srcset="../assets/images/photos/psol-image-800w.jpg">
           <img src="../assets/images/photos/psol-image-800w.jpg" alt="PSOL">
         </picture>
-        <p> <strong>Campanha de comunicação.</strong>
+        <p>
+          <strong>Campanha de comunicação.</strong>
           A construção de uma campanha online que fure o bloqueio da grande mídia é essencial para fazer com que essas propostas cheguem ao máximo possível de pessoas. Para isso, vamos contratar equipe qualificada em redes sociais e comprar os equipamentos necessários para transmissões ao vivo dos eventos, cobertura fotográfica e produção de conteúdos de divulgação.
         </p>
-        <p><strong>Sistematização das propostas feitas.</strong>
+        <p>
+          <strong>Sistematização das propostas feitas.</strong>
           Durante a realização dos eventos e dos debates em nossa plataforma online, será preciso consolidar todas as propostas realizadas em eixos concretos para apresentar o programa ao país. Assim, é necessário ter pessoas dedicadas à sistematização da ampla gama de propostas e ideias surgidas, bem como viabilizar a apresentação e divulgação delas para todas e todos que participaram do processo.
         </p>
       </div>
     </article>
-
 
     <!-- <article id="home__faq" class="home__faq">
       <div class="container" id="donation-wrap">
@@ -87,21 +83,21 @@
           Doadores
         </h2>
 
-		<ul class="faq-group">
-			<li>
-				<a class="faq-question" href="#0">Como faço doação?</a>
-				<div class="faq-content">
-					<p>Para fazer a doação...</p>
-				</div>
-			</li>
+    <ul class="faq-group">
+      <li>
+        <a class="faq-question" href="#0">Como faço doação?</a>
+        <div class="faq-content">
+          <p>Para fazer a doação...</p>
+        </div>
+      </li>
 
-			<li>
-				<a class="faq-question" href="#0">Meus dados estão protegidos?</a>
-				<div class="faq-content">
-					<p>Exemplo de resposta</p>
-				</div>
-			</li>
-		</ul>
+      <li>
+        <a class="faq-question" href="#0">Meus dados estão protegidos?</a>
+        <div class="faq-content">
+          <p>Exemplo de resposta</p>
+        </div>
+      </li>
+    </ul>
       </div>
     </article> -->
   </main>
@@ -136,7 +132,6 @@ export default {
           return this.candidate.raising_goal;
         }
       }
-
       return 0
     },
     porcentage() {
@@ -145,8 +140,8 @@ export default {
         return Math.ceil(value);
       }
 
-      return 0
-    }
+      return 0,
+    },
   },
 };
 </script>
