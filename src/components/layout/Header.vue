2a9d623e05f1eea18c0a1fdfbe7c80f36634d7bb
Evelyn Perez <evelyn@eokoe.com>
2018-09-19 18:05:15 -0300

    Update header content

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,80 +1,78 @@
 <template>
-	<header class="site-header">
-		<nav class="skip-links">
-			<ul>
-				<li class="to-content"><a href="#conteudo">conteúdo</a></li>
-				<li class="to-menu"><a href="#menu">menu</a></li>
-			</ul>
-		</nav>
-		<nav class="main-menu">
-			<div class="menu-container">
-				<button class="mobile-button">
-					<span></span>
-					<span></span>
-					<span></span>
-				</button>
-				<div class="brand-wrap">
-					<a href="#home__greetings" v-scroll-to="'#home__greetings'" class="logo-psol">PSOL</a>
-				</div>
-				<ul class="menu-wrap">
-					<li class="menu-item">
-						<a href="#home__knowMore" v-scroll-to="'#home__knowMore'">
-							Uso dos recursos
-						</a>
-					</li>
-					<li class="menu-item donate-nav">
-						<a href="#doar" v-scroll-to="'#doar'">
-							Doar
-						</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-		<div id="home__greetings">
-			<div class="site-label">
-				<h1><span>Vamos</span> juntos</h1>
-				<h1>Mudar o <span>Brasil</span>!</h1>
-				<a class="call-to-action" href="#doar" v-scroll-to="'#doar'">
-					<span>#</span>FINANCIE<span class="heavy">PSOL</span>
-				</a>
-			</div>
-		</div>
-	</header>
+  <header class="site-header">
+    <nav class="skip-links">
+      <ul>
+        <li class="to-content"><a href="#conteudo">conteúdo</a></li>
+        <li class="to-menu"><a href="#menu">menu</a></li>
+      </ul>
+    </nav>
+    <nav class="main-menu">
+      <div class="menu-container">
+        <button class="mobile-button">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <div class="brand-wrap">
+          <a href="#home__greetings" v-scroll-to="'#home__greetings'" class="logo-psol">PSOL</a>
+        </div>
+        <ul class="menu-wrap">
+          <li class="menu-item">
+            <a href="#home__knowMore" v-scroll-to="'#home__knowMore'">
+              Uso dos recursos
+            </a>
+          </li>
+          <li class="menu-item donate-nav">
+            <a href="#doar" v-scroll-to="'#doar'">
+              Doar
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div id="home__greetings">
+      <div class="site-label">
+        <h1>Quem financia o <span>PSOL</span> é você</h1>
+          <a class="call-to-action" href="#doar" v-scroll-to="'#doar'">
+          Quero doar >
+          </a>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-	name: 'Header',
+  name: 'Header',
 
-	methods: {
-		scrollMenu() {
-			const element = document.querySelector('.main-menu');
+  methods: {
+    scrollMenu() {
+      const element = document.querySelector('.main-menu');
 
-			window.addEventListener('scroll', (e) => {
-				const newOffset = window.scrollY;
-				const currentRouteName = this.$route.name;
-					if (newOffset > 13 && window.matchMedia("").matches) {
-						element.classList.add('animate');
-
-					} else {
-						element.classList.remove('animate');
-					}
-			});
-		},
-		showMenu() {
-			const element = document.querySelector('.mobile-button');
-			const menu = document.querySelector('.menu-wrap');
-			element.addEventListener('click', function() {
-				menu.classList.toggle('show');
-			})
-		}
-	},
-	created() {
-		this.$nextTick(function() {
-			this.scrollMenu();
-			this.showMenu();
-		});
-	}
+      window.addEventListener('scroll', (e) => {
+        const newOffset = window.scrollY;
+        const currentRouteName = this.$route.name;
+        if (newOffset > 13 && window.matchMedia("").matches) {
+          element.classList.add('animate');
+        } else {
+          element.classList.remove('animate');
+        }
+      });
+    },
+    showMenu() {
+      const element = document.querySelector('.mobile-button');
+      const menu = document.querySelector('.menu-wrap');
+      element.addEventListener('click', function() {
+        menu.classList.toggle('show');
+      })
+    }
+  },
+  created() {
+    this.$nextTick(function() {
+      this.scrollMenu();
+      this.showMenu();
+    });
+  }
 }
 </script>
 
